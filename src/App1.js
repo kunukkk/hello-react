3ae6d9ebbe4448c5881113b508abf605c792781f
kunukkk <showklee@naver.com>
2020-10-30 20:56:22 +0900

    20/10/30_add
    20/10/30_add

---
+++ src/App1.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App1 extends Component {
+  render() {
+    const text = '당신은 어썸한가요?';
+    const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    };
+
+    return (
+      <div className="my-div">
+        { /* 요소 밖에서는 주석에 {} 필요 */ }
+        <h1>리액트 안녕!</h1>
+        <h2>{text}</h2>
+        {condition && '보여주세요'}
+        <div
+          style={style}
+          // self-closed 태그에서만 작동하는 주석
+          // 마지막 />가 꼭 새 줄에 있어야 한다
+          /**/
+        />
+        // 여기서 쓰는 주석은 그대로 렌더링
+        /**/
+      </div>
+    )
+  }
+}
+
