1bbd5f3a75b72fc929014807e92bc15f3e16f773
kunukkk <showklee@naver.com>
2020-11-16 17:46:08 +0900

    20/11/09_review
    20/11/09_review

--- src/App5.js
+++ src/App5.js
@@ -5,7 +5,7 @@ class App5 extends Component {
     render() {
         return (
             <div>
-                <ScrollBox ref={(ref) => {this.scrollBox=ref}} />
+                <ScrollBox ref={(ref) => {this.scrollBox=ref}} /> { /* ex) this.scrollBox = document.getElementById("ScrollBox") */ }
                 <button onClick={() => {this.scrollBox.scrollToBottom()}}> { /* onclick={this.scrollBox.scrollToBottom 으로 작성 시 오류발생*/ }
                     맨 밑으로
                 </button>
