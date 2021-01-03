50867f74d24ae84fced60311bef6fdc6ce54e6ec
ukunV <showklee@naver.com>
2021-01-03 19:28:44 +0900

    21/01/03_add
    21/01/03_add

---
+++ src/App6.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import IterationSample from './IterationSample';
+
+class App6 extends Component {
+    render() {
+        return (
+            <IterationSample />
+        );
+    }
+}
+
+export default App6;
