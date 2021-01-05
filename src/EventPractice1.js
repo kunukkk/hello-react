f8eebd524eddbb67b092bf4e008febcea8c711f0
ukunV <showklee@naver.com>
2021-01-05 20:28:42 +0900

    21/01/05_update
    21/01/05_update

--- src/EventPractice1.js
+++ src/EventPractice1.js
@@ -1,40 +1,91 @@
 import React, { Component } from 'react';
 
 class EventPractice1 extends Component {
-    state = {
-        message: ''
-    }
+  // constructor(props) {
+  //     // super(props): super는 부모 클래스 생성자를 가리키는데 여기서는 Component
+  //     // super(props)를 선언하기 전에는 constructor에서 this를 사용할 수 없다
+  //     super(props);
+  //     // bind: 컴포넌트에 임의 메서드를 만들면 기본적으로는 this에 접근할 수 없다
+  //     // 따라서 컴포넌트의 생성자 메서드인 constructor에서 각 메서드를 this와 바인딩(binding) 해준다
+  //     this.handleChange = this.handleChange.bind(this);
+  //     this.handleClick = this.handleClick.bind(this);
+  // }
 
-    render() {
-        return (
-            <div>
-                <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="아무거나 입력해보세요"
-                    value={this.state.message}
-                    onChange={
-                        (e) => {
-                            console.log(e);
-                            console.log(e.target.value);
-                            console.log(this.state.message);
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
-                />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        })
-                    }
-                }
-                >확인</button>
-            </div>
-        );
+  // handleChange(e) {
+  //     this.setState({
+  //         message: e.target.value
+  //     })
+  // }
+
+  // handleClick() {
+  //     alert(this.state.message);
+  //     this.setState({
+  //         message: ''
+  //     })
+  // }
+
+  state = {
+    username: '',
+    message: '',
+  };
+
+  handleChange = (e) => {
+    // e: event객체
+    this.setState({
+      // e.target.name은 해당 input의 name을 가르킨다
+      [e.target.name]: e.target.value,
+      // 객체 안에서 key를 감싸면 그 안에 넣은 레퍼런스가 가리키는 실제 값이 key 값으로 사용된다.
+      /*
+      ex)
+      const name = 'variantKey';
+      const object = {
+        [name]: 'value'
+      }
+
+      >> {
+        'variantKey': 'value'
+      }
+      */
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.username + ': ' + this.state.message);
+    this.setState({
+      username: '',
+      message: '',
+    });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleClick();
     }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="USERNAME"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="MESSAGE"
+          value={this.state.message}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
 }
+
+export default EventPractice1;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'; // 컴포넌트 필수 props를 지정하거나 props 타입(type)을 지정할 때 이용
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class MyComponent extends Component {
-    static defaultProps = { // default props값 설정
-        name: '기본 이름'
-    }
+const MyComponent = ({ name, children, favoriteNumber }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}입니다. <br />
+      {/* childern: 리액트 컴포넌트를 사용할 때 컴포넌트 태그 사이의 내용을 보여 주는 props */}
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
+    </div>
+  );
+};
 
-    static propTypes = {
-        name: PropTypes.string, // name props 타입을 문자열로 설정합니다.
-        age: PropTypes.number.isRequired // 필수적으로 존재해야 하며, 숫자입니다.
-    }
+// const MyComponent = (props) => {
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {props.name}입니다. <br />
+//       children 값은 {props.children}입니다.
+//       {/* childern: 리액트 컴포넌트를 사용할 때 컴포넌트 태그 사이의 내용을 보여 주는 props */}
+//     </div>
+//   );
+// };
 
-    state = {
-        number: 0
-    }
+MyComponent.defaultProps = {
+  name: '기본 이름',
+};
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         number: 0
-    //     }
-    // }
-
-    render() {
-        return (
-            <div>
-                <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p> {/* props: 부모 컴포넌트에서 설정 (현재: App.js) */}
-                <p>저는 {this.props.age}살 입니다.</p>
-                <p>숫자: {this.state.number}</p>
-                <button onClick={() => { // 일반 함수는 자신이 종속된 객체를 this로 가리키며, 화살표 함수는 자신이 종속된 인스턴스를 가르킨다.
-                    this.setState({ // setState() 메서드의 역할은 파라미터로 전달받은 필드를 업데이트한 후 컴포넌트가 리렌더링하도록 트리거하는 것
-                        number: this.state.number + 1
-                    })
-                }}>더하기</button>
-            </div>
-        );
-    }
-}
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
 
 export default MyComponent;
